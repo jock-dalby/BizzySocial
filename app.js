10de88b0ab41060e7151e29281a54af08e5a1201
jock-dalby <jockdalby86@gmail.com>
2016-09-15 08:38:55 +1200

    Setup basic folder structure and main, login and reg views

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+const path = require('path')
 const app = express()
 
+
+// view engine setup
+app.engine('hbs', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'hbs')
+
+// serve the files in /public
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Routes
 app.get('/', function(req, res){
-  res.send("Hello, I'm working")
+  res.render('login', {title:"Bizzy Login"})
+})
+
+app.get('/register', function(req, res){
+  res.render('register', {title:"Bizzy Page"})
 })
 
 
-// Exports app functions
 
+
+
+// Exports app functions
 module.exports = app

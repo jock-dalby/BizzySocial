ba8f0ef93aa74de260c57895f0d37191ef0afab5
jock-dalby <jock-dalby@jock-dalby.local>
2016-09-17 08:51:14 +1200

    Rollback complete

--- db/follow.js
+++ db/follow.js
@@ -1,67 +1,43 @@
 module.exports =
 
 [{
-    user_id: 1,
-    follower: 3
+    user_id: 10,
+    follower: "[10, 2, 4, 6]"
+},
+{
+    user_id: 8,
+    follower: "[8, 1, 2, 3]"
 },
 {
     user_id: 6,
-    follower: 2
-},
-{
-    user_id: 9,
-    follower: 4
-},
-{
-    user_id: 8,
-    follower: 2
-},
-{
-    user_id: 8,
-    follower: 6
-},
-{
-    user_id: 8,
-    follower: 3
+    follower: "[6, 7, 8, 9]"
 },
 {
     user_id: 4,
-    follower: 2
+    follower: "[4, 5, 8, 11]"
 },
 {
-    user_id: 5,
-    follower: 9
+    user_id: 2,
+    follower: "[2, 10, 4, 1]"
 },
 {
-    user_id: 8,
-    follower: 4
+    user_id: 11,
+    follower: "[11, 8, 2, 6]"
 },
 {
     user_id: 7,
-    follower: 1
+    follower: "[7, 4, 3, 9]"
+},
+{
+    user_id: 5,
+    follower: "[5, 6, 2, 10]"
+},
+{
+    user_id: 3,
+    follower: "[3, 7, 1, 4]"
 },
 {
     user_id: 1,
-    follower: 7
-},
-{
-    user_id: 1,
-    follower: 9
-},
-{
-    user_id: 9,
-    follower: 6
-},
-{
-    user_id: 8,
-    follower: 9
-},
-{
-    user_id: 10,
-    follower: 3
-},
-{
-    user_id: 10,
-    follower: 7
-},
+    follower: "[1, 9, 5, 2]"
+}
 ]

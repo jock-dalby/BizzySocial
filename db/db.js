fe2dd55bec2790175535f9ce76e6cd2597137338
jock-dalby <jockdalby86@gmail.com>
2016-09-18 13:40:46 +1200

    Update db.js

--- db/db.js
+++ db/db.js
@@ -42,7 +42,7 @@ module.exports = {
     .then(function(rows){
       user.push(rows[0])
       callback(null, user)
-      return knex.insert({user_id: userId, post: JSON.stringify(newPost)}).into('posts')
+      return knex.insert({user_id: userId, post: newPost}).into('posts')
     })
     .catch(function(err){
       callback(err)

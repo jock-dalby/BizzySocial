82971dc02cc59954057e42d815702edb0c6b2b92
jock-dalby <jockdalby86@gmail.com>
2016-09-19 22:06:37 +1200

    Update db.js

--- db/db.js
+++ db/db.js
@@ -3,50 +3,19 @@ const _ = require('lodash')
 
 module.exports = {
 
-  findUserByName: function(userName, callback) {
-    var userFound = []
-    knex('users')
-    .select('userName', 'id', 'logo')
-    .then(function(users) {
-      userFound[0] = _.find(users, function(c){
-      return c.userName === userName
-    })
-    callback(null, userFound)
-    })
-    .catch(function(err){
-      callback(err)
-    })
+  findUserByName: function(userName) {
+    return knex('users').where({'userName':userName}).select('userName', 'id', 'logo')
   },
 
-  addNewUser: function(userDetails, callback) {
+  addNewUser: function(userDetails) {
     var newUser = []
-    knex('users')
-    .then(function(){
-      newUser[0] = userDetails
-      delete newUser[0].commit
-      knex('users').insert(newUser)
-    .then(function(){
-        return callback(null, newUser)
-      })
-    })
-    .catch(function(err){
-      callback(err)
-    })
+    newUser[0] = userDetails
+    delete newUser[0].commit
+    return knex('users').insert(newUser)
   },
 
-  addPost: function(userId, newPost, callback) {
-    var user = []
-    knex('users')
-    .where({'id': userId})
-    .select('userName', 'id', 'logo')
-    .then(function(rows){
-      user.push(rows[0])
-      callback(null, user)
-      return knex.insert({user_id: userId, post: newPost}).into('posts')
-    })
-    .catch(function(err){
-      callback(err)
-    })
+  addPost: function(userId, newPost) {
+    return knex.insert({user_id: userId, post: newPost}).into('posts')
   }
 
 }
